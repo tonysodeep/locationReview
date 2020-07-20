39fcdb561799ebda14bbc602d5fc12bed2025e57
hynguyen <hynguyen2727@gmail.com>
2020-07-20 09:41:05 +0700

    edit location

---
+++ app/src/main/java/com/example/app_day_5/EditLocation.kt
@@ -0,0 +1,12 @@
+package com.example.app_day_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditLocation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_location)
+    }
+
+}
